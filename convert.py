244df24cd4be60294bc76fed6373b841de98a4b0
Nishant Gaurav <iamnishantg@gmail.com>
2016-03-04 08:54:32 +0530

    its working

--- convert.py
+++ convert.py
@@ -3,9 +3,9 @@ from subprocess import call
 
 for subdir, dirs, files in os.walk(os.getcwd()):
     for f in files:
-    	if  f.endswith(".py"): 
-     		print("dirs:",dirs,"subdir:",subdir,"files",files)
-     		#call(["2to3", "-w", f])
-        	continue
-    	else:
-        	continue
+        if  f.endswith(".py"):
+            print("dirs:",dirs,"subdir:",subdir,"files",files)
+            #call(["2to3", "-w", f])
+            continue
+        else:
+            continue
