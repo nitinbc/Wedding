9a29d62c9caef52ee07a9c8697f6e1e4f1c74638
Nishant Gaurav <iamnishantg@gmail.com>
2016-03-05 15:26:17 +0530

    logging moved to .env

--- bootcamp/settings.py
+++ bootcamp/settings.py
@@ -4,6 +4,7 @@ PROJECT_DIR = Path(__file__).parent
 from decouple import config
 
 import dj_database_url
+import logging
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -102,33 +103,5 @@ ALLOWED_SIGNUP_DOMAINS = ['*']
 
 FILE_UPLOAD_TEMP_DIR = '/tmp/'
 
-import logging
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file1': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/home/ubuntu/public_html/logs/django.log',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers':['file1'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-    }
-}
+LOGGING = config('LOGGING')
